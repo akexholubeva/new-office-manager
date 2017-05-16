a0fc074cd3524982c70a343a7a21ed563fece027
Alex Golubeva <sasha-96hi@mail.ru>
2017-05-16 15:38:57 +0300

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,44 @@
+const webpack=require('webpack');
+const path=require('path');
+const HtmlWebpackPlugin=require('html-webpack-plugin');
+
 module.exports = {
-    entry: './src/app.js',
+    context: path.resolve(__dirname, './src'),
+    entry: './src/index.js',
     output: {
+        path:path.resolve(__dirname, './dist'),
         filename: './dist/bundle.js',
     },
     resolve: {
         extensions: [".js", ".jsx"]
     },
+    plugins:[
+        new HtmlWebpackPlugin({
+            title:'New office-manager',
+            filename: './dist/index.html',
+        })
+    ],
+    
     module:{
-        loaders: [{
+        rules: [{
              test: /\.jsx?$/,
              exclude: /node_modules/,
-             loader: 'babel-loader'
-            },
-            { 
+             use: 'babel-loader'
+        }, {
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            use:'eslint-loader'
+        }, { 
              test: /\.css$/,
-             loader: ["style", "css"]
-            }
-         ]
-
+             use: ["style-loader", "css-loader"]
+        }, {
+             test: /\.(jpe?g|png|gif)$/,
+             exclude: /(node_modules)/,
+             use: 'url-loader?limit=10000'
+        }, {
+            test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+            use: "file-loader"
+        }
+        ],
     },
-
 }
